bdb87ca982430ddd3145cbd20c3ee5fc27f02ccf
himky <himky@noreply.github.com>
2021-08-24 23:29:21 +0900

    wip

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.debug('hello')
+import { createElement } from 'react';
+import { render } from 'react-dom';
+
+function MyApplication() {
+  return createElement('div', {}, 'Hello world');
+}
+
+const rootElement = document.querySelector('.app');
+render(createElement(MyApplication), rootElement);
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
 const path = require('path')
+const { WebpackBundleSizeAnalyzerPlugin } = require('webpack-bundle-size-analyzer')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+const mode = process.env.APP_ENV || 'production'
 const htmlWebpackPlugins = ['index.html'].map(filename => new HtmlWebpackPlugin({ filename, template: `src/html/${filename}`, inject: true }))
 
 module.exports = {
-  mode: process.env.NODE_ENV || 'production',
+  mode,
   entry: './src/index.js',
-  output: { path: path.resolve(__dirname, 'dist') },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
   plugins: [
-    ...htmlWebpackPlugins
+    ...htmlWebpackPlugins,
+    new WebpackBundleSizeAnalyzerPlugin('./bundle-size-list.md')
   ]
 }
